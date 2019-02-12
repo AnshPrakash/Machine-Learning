f799a1673890581ba3fc91b711c4f1e5303ad1cf
AnshPrakash <anshprakash1997@gmail.com>
2019-02-12 14:48:30 +0530

    Some Plots done

--- LabAssignments/1/LinearReg.py
+++ LabAssignments/1/LinearReg.py
@@ -1,5 +1,9 @@
-import sys
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import matplotlib.animation as animation
+import time
+plt.style.use('seaborn-white')
 
 
 filex=open("./data/linearX.csv","r")
@@ -9,31 +13,121 @@ labels=np.array([float(y) for y in filey])
 X=np.array([float(x) for x in filex])
 labels=labels.reshape((np.shape(labels)[0]),1)
 X=X.reshape((np.shape(X)[0],1))
+x_std=np.std(X)+0.001
+x_mean=np.std(X)+0.001
+X=(X-x_mean)*(1/x_std)
+# y_std=np.std(labels)+0.001
+# y_mean=np.mean(labels)+0.001
+# labels=(labels-y_mean)*(1/(y_mean))
 X=np.hstack((np.ones(np.shape(X)[0]).reshape((np.shape(X)[0],1)),X))
+# print(X)
 
-
-
-def cost(theta):
+def cost(theta0,theta1):
+	theta=np.array([(theta0),(theta1)]).reshape((2,1))
 	tp=(X.dot(theta)-labels)
 	cost=(tp.T).dot(tp)
-	return(float(cost))
+	return(float(cost)/(2*np.shape(X)[0]))
+
+
 
 
 def BGD(X,labels):
 	theta=np.zeros(np.shape(X)[1]).reshape((np.shape(X)[1],1))
 	diff=10*np.ones(np.shape(X)[1]).reshape((np.shape(X)[1],1))
-	epsilon=0.00001
+	epsilon=0.0000001
 	diff=float("inf")
-	learining_rate=0.01
+	learining_rate=0.1
 	m=(np.shape(X)[0])
+	pcost=cost(theta[0],theta[1])
+	mem=[(theta,pcost)]
 	while diff>epsilon:
 		temp=(X.T).dot(labels-X.dot(theta))
 		theta=theta+(learining_rate/m)*temp
-		temp=abs(temp)
-		diff=float(temp.max(axis=0))*(learining_rate/m)
-	return(theta)
+		new_cost=cost(theta[0],theta[1])
+		diff=abs(new_cost- pcost)
+		pcost=new_cost
+		mem.append((theta,pcost))
+	return(theta,mem)
 
 
-theta=BGD(X,labels)
-print(cost(theta))
-# print(theta)
+theta,mem=BGD(X,labels)
+print(cost(theta[0],theta[1]))
+
+############  PLOTS FROM HERE ############################
+
+# Part(a)
+pltx=np.linspace(0,20,50).reshape((50,1))
+pltx=np.hstack((np.ones(np.shape(pltx)[0]).reshape((np.shape(pltx)[0],1)),pltx))
+plty=np.array([(theta.T).dot(pltx[i]) for i in range(np.shape(pltx)[0])])
+
+fig1, ax1 = plt.subplots()
+ax1.plot(pltx[:,1],plty,'-')
+ax1.plot(X[:,1],labels,'o')
+ax1.legend(['hypothesis','Original Data'], loc='upper left')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+plt.title('Hypothesis function')
+
+# Part(b)
+steps=50
+theta0=np.linspace(-2,2,steps).reshape((steps,1))
+theta1=np.linspace(-2,2,steps).reshape((steps,1))
+th0_mesh,th1_mesh=np.meshgrid(theta0,theta1)
+H=[[0]*steps for _ in range(steps)]
+
+for i in range(steps):
+	for j in range(steps):
+		H[i][j]=cost(th0_mesh[i][j],th1_mesh[i][j])
+H=np.array(H)
+
+
+
+fig2=plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.set_xlabel('theta0')
+ax2.set_ylabel('theta1')
+ax2.set_title('COST FUNCTION CONTOURS')
+plt.contour(th0_mesh,th1_mesh,H, colors='blue')
+
+
+x,y,z=[],[],[]
+for i in range(len(mem)):
+	x.append(float(mem[i][0][0]))
+	y.append(float(mem[i][0][1]))
+	z.append(mem[i][1])
+
+plt.plot(x,y,'o')
+	# iter+=1
+
+
+
+
+
+
+fig3 = plt.figure()
+ax = fig3.add_subplot(111,projection='3d')
+ax.set_xlabel('theta0')
+ax.set_ylabel('theta1')
+ax.set_zlabel('Cost function J(ϴ)')
+ax.set_title('COST FUNCTION')
+ax.plot_surface(th0_mesh,th1_mesh,H, rstride=1, cstride=1, edgecolor='none')
+plt.plot(x,y,'o')
+# def animate(i,mem):
+# 	global elements
+# 	x,y,z=[],[],[]
+# 	for i in range(len(mem)):
+# 		x.append(float(mem[i][0][0]))
+# 		y.append(float(mem[i][0][1]))
+# 		z.append(mem[i][1])
+# 		time.sleep(2)
+# 	if elements>=len(mem):
+# 		elements=1
+# 	ax.plot(x,y,z,'^',color='r')
+# 	ax.clear()
+# 	ax.plot_surface(th0_mesh,th1_mesh,H, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+
+# ani=animation.FuncAnimation(fig,animate,fargs=(mem),interval=2000)
+
+
+# plt.plot(x,y,'^',color='b')
+plt.show()
--- LabAssignments/1/LocallyWeightedLR.py
+++ LabAssignments/1/LocallyWeightedLR.py
@@ -1,6 +1,9 @@
 import sys
 import numpy as np
 from math import *
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import matplotlib.animation as animation
 
 
 filex=open("./data/weightedX.csv","r")
@@ -12,7 +15,35 @@ Y=np.array([float(y) for y in filey])
 X=np.array([float(x) for x in filex])
 Y=Y.reshape((np.shape(Y)[0]),1)
 X=X.reshape((np.shape(X)[0],1))
+x_std=np.std(X)+0.001
+x_mean=np.std(X)+0.001
+X=(X-x_mean)*(1/x_std)
 X=np.hstack((np.ones(np.shape(X)[0]).reshape((np.shape(X)[0],1)),X))
+
+
+def cost(theta0,theta1):
+	theta=np.array([(theta0),(theta1)]).reshape((2,1))
+	tp=(X.dot(theta)-Y)
+	cost=(tp.T).dot(tp)
+	return(float(cost)/(2*np.shape(X)[0]))
+
+
+
+def BGD(X,labels):
+	theta=np.zeros(np.shape(X)[1]).reshape((np.shape(X)[1],1))
+	diff=10*np.ones(np.shape(X)[1]).reshape((np.shape(X)[1],1))
+	epsilon=0.000001
+	diff=float("inf")
+	learining_rate=0.1
+	m=(np.shape(X)[0])
+	pcost=cost(theta[0],theta[1])
+	while diff>epsilon:
+		temp=(X.T).dot(labels-X.dot(theta))
+		theta=theta+(learining_rate/m)*temp
+		new_cost=cost(theta[0],theta[1])
+		diff=abs(new_cost- pcost)
+		pcost=new_cost
+	return(theta)
 
 
 
@@ -20,12 +51,12 @@ X=np.hstack((np.ones(np.shape(X)[0]).reshape((np.shape(X)[0],1)),X))
 def weightMatrix(x,X):
 	m=np.shape(X)[0]
 	t=0.8 ##Std deiviation
-	W=[ ((x-X[i]).T).dot((x-X[i]).T) for i in range((m))]
+	W=[float(((x-X[i].reshape(np.shape(X)[1],1)).T).dot((x-X[i].reshape(np.shape(X)[1],1)))) for i in range((m))]
 	W=np.array(W)
 	W=(W*(-1))/(2*t*t)
 	temp=[[0]*m for _ in range(m)]
 	for i in range(m):
-		temp[i][i]=exp(W[i])
+		temp[i][i]=np.exp(W[i])
 	return(np.array(temp))
 
 
@@ -35,4 +66,37 @@ def wBGD(X,Y,x):
 	theta=((((np.linalg.inv(((X.T).dot(W)).dot(X))).dot(X.T)).dot(W.T+W)).dot(Y))*0.5
 	return(theta)
 
-print(wBGD(X,Y,X[0].T))
+
+############  PLOTS FROM HERE ############################
+
+# 2Part(a)
+theta_un_weighted=BGD(X,Y)
+print(theta_un_weighted)
+pltx=np.linspace(-5,5,50).reshape((50,1))
+pltx=np.hstack((np.ones(np.shape(pltx)[0]).reshape((np.shape(pltx)[0],1)),pltx))
+plty=np.array([(theta_un_weighted.T).dot(pltx[i]) for i in range(np.shape(pltx)[0])])
+
+fig1, ax1 = plt.subplots()
+ax1.plot(pltx[:,1],plty,'-')
+ax1.plot(X[:,1],Y,'o')
+ax1.legend(['hypothesis','Original Data'], loc='upper left')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+plt.title('Hypothesis function(Linear regression)')
+
+
+# Part(b)
+fig2,ax2=plt.subplots()
+pltx=np.linspace(-5,5,50).reshape((50,1))
+pltx=np.hstack((np.ones(np.shape(pltx)[0]).reshape((np.shape(pltx)[0],1)),pltx))
+plty=np.array([(wBGD(X,Y,pltx[i].reshape(2,1)).T).dot(pltx[i]) for i in range(np.shape(pltx)[0])])
+
+ax2.plot(pltx[:,1],plty,'-')
+ax2.plot(X[:,1],Y,'o')
+ax2.legend(['hypothesis','Original Data'], loc='upper left')
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
+plt.title('Hypothesis function(Locally Weighted Linear regression)')
+plt.show()
+
+
