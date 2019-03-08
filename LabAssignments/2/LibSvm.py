4a637cf01df0854b6a7edc6bce9d944d85b1fc14
AnshPrakash <anshprakash1997@gmail.com>
2019-03-09 03:16:50 +0530

    LibSVM for Binary Classification Done

---
+++ LabAssignments/2/LibSvm.py
@@ -0,0 +1,46 @@
+# Python2
+import time 
+import numpy as np
+import pandas as pd
+from svmutil import *
+
+
+
+entry_no = 7
+# start_time = time.time()
+with open('./mnist/train.csv') as f:
+	df = pd.read_csv(f,header=None)
+
+df1 = (df.loc[(df.iloc[:,-1] == entry_no) | (df.iloc[:,-1] == (entry_no+1)%10)])
+
+df1.iloc[:,-1]=df1.iloc[:,-1].replace({entry_no:1,(entry_no+1)%10:-1})
+
+Y = np.array(df1.iloc[:,-1])
+X = np.array(df1.iloc[:,:-1])
+m = len(Y)
+Y = Y.reshape(m,1)
+C = 1.0
+n = 28*28 #28 X 28 dimension images
+
+Y_p = list(df1.iloc[:,-1])
+X_p = df1.values.tolist()
+
+svm_model.predict = lambda self,y,x: svm_predict(y, x, self)[0][0]
+prob = svm_problem(Y_p,X_p)
+param = svm_parameter('-s 0 -t 0 -c '+str(C)) ## Linear SVM 
+param.kernel_type = LINEAR 
+param.C = C
+
+model = svm_train(prob, param)
+
+b = -model.rho.contents.value ## seen from documentation
+
+training_accuracy = model.predict(Y_p,X_p)
+support_vector_indices = model.get_sv_indices()
+alphas = model.get_sv_coef() #corresponding to support vector indices other alphas are zeros
+w = (X[np.array(support_vector_indices)-1].T).dot(Y[np.array(support_vector_indices)-1]*np.array(alphas).reshape(len(alphas),1))
+if model.get_labels()[1] == -1:  # just to make vector point to positive examples
+    w = -w
+    b = -b
+print(training_accuracy)
+
--- LabAssignments/2/SVM.py
+++ LabAssignments/2/SVM.py
@@ -1,3 +1,4 @@
+# Python 3
 import time 
 import numpy as np
 import pandas as pd
@@ -11,13 +12,14 @@ with open('./mnist/train.csv') as f:
 
 df1=(df.loc[(df.iloc[:,-1]==entry_no) | (df.iloc[:,-1]==(entry_no+1)%10)])
 
+df1.iloc[:,-1]=df1.iloc[:,-1].replace({entry_no:1,(entry_no+1)%10:-1})
 
 Y = np.array(df1.iloc[:,-1])
 X = np.array(df1.iloc[:,:-1])/255
 m=len(Y)
 C = 1.0
 Y=Y.reshape(m,1)
-n=28*28
+n=28*28 #28 X 28 dimension images
 
 print(m)
 
@@ -33,8 +35,18 @@ b = matrix(np.zeros((1,1)))
 sol=solvers.qp(P,q,G,h,A,b)
 alphas=np.array(sol['x'])
 
-w=(X.T)@alphas
+w=(X.T).dot(alphas*Y)
 
 
+# get the support vector
+def getSupportVectors(e=1e-4):
+     global alphas
+     vec=[]
+     for i in range(m):
+             if abs(alphas[i])>e:
+                     vec.append(i)
+     return(vec)
 
-# linearKernelSVM()
+
+# Using LIBSVM
+
